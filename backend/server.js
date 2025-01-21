d89a5fb8334c531c624df28572c6b3b40ec5d53b
jmei403 <jason.mei403@gmail.com>
2025-01-21 16:55:20 -0500

    rename client and server folders, set up Express server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+app.get('/', (req, res) => res.send('Server is ready'));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
